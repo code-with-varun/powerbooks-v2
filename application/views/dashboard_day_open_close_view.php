e45d62127cb1401dbd258ef8848826bcf817e1c5
varunakshay_trainee <varunakshay@example.com>
2024-02-19 10:58:51 +0530

    Dayclose alert message

--- application/views/dashboard_day_open_close_view.php
+++ application/views/dashboard_day_open_close_view.php
@@ -40,23 +40,43 @@ $data['merchant_id']= $sessdata['pbk_merchant_id'];
                             </ul>
                         </div>
                         <div class="body">
-                          <?php 
+                        <?php 
+    $config_master_fetch = $this->Users_model->config_master_fetch($data);
+    foreach ($config_master_fetch as $row) {
+        $auto_day_end = $row->auto_day_end;
+        $pos_status = $row->pos_status;
+        $current_pos_date = $row->current_pos_date;
+    }
 
-						$config_master_fetch = $this->Users_model->config_master_fetch($data);
-		foreach ($config_master_fetch as $row)
-		{
-			$auto_day_end = $row->auto_day_end;
-			$pos_status = $row->pos_status;
-			$current_pos_date = $row->current_pos_date;
-		}
+    if ($pos_status == "OPENED") {
+        $change_status = "CLOSED";
+        $pos_status = "OPENED";
+        $btn_color = "pink";
+    } else {
+        $change_status = "OPENED";
+        $pos_status = "CLOSED";
+        $btn_color = "green";
+    }
+    
+    echo '<center><h4> Billing ' . $pos_status . ' for date ( ' . date('d-M-Y - l', strtotime($current_pos_date)) . ' )<br><br>';
+    
+    if ($auto_day_end != 'YES') {    
+        echo '<form id="posForm" action="update-pos" method="POST"><input type="hidden" name="change_status" value="'.$change_status.'">';
+        echo '<button type="button" class="btn bg-'.$btn_color.' waves-effect" onclick="confirmPosStatusChange()">Click Here</button> to '.$change_status.' POS.</h4></center></form>';
+    }
+?>
 
-		if($pos_status=="OPENED"){$change_status="CLOSED";$pos_status="OPENED"; $btn_color="pink";}
-		else{$change_status="OPENED";$pos_status="CLOSED"; $btn_color="green";}
-		echo '<center><h4> Billing '.$pos_status.' for date ( '.date('d-M-Y - l',strtotime($current_pos_date)).' )<br><br>';
-		if($auto_day_end!='YES'){	
-		echo'<form action="update-pos" method="POST"><input type="hidden" name="change_status" value="'.$change_status.'"><button type="submit" class="btn bg-'.$btn_color.' waves-effect">Click Here</button> to '.$change_status.' POS. </h4></center></form>';
-		}
-		?> 
+<script>
+    function confirmPosStatusChange() {
+        if (confirm("Are you sure you want to change the POS status to <?php 
+            echo $change_status.
+            ' for date ( ' . date('d-M-Y - l', strtotime($current_pos_date)).' )';
+            ?>?")) {
+            document.getElementById("posForm").submit();
+        }
+    }
+</script>
+
                           
                             
                         </div>
